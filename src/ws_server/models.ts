e1f8562c49565bdabc039385482690e7765d5b4d
KirillDemyanenko <demyanenkokg@gmail.com>
2023-07-10 22:15:10 +0300

    fix: User model

--- src/ws_server/models.ts
+++ src/ws_server/models.ts
@@ -1,3 +1,13 @@
+import WebSocket from "ws";
+
 export class User {
-    constructor(public name: string, public password: string) {}
+    constructor(public name: string, public password: string, public ws: WebSocket) {}
+
+    updateWS (ws) {
+        this.ws = ws
+    }
+
+    getWs () {
+        return this.ws
+    }
 }
