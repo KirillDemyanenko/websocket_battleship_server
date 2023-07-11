8939e14fd02994a3d6b13dc3550908fcc760867d
KirillDemyanenko <demyanenkokg@gmail.com>
2023-07-11 12:55:44 +0300

    refactor: function typing

--- src/ws_server/rooms.ts
+++ src/ws_server/rooms.ts
@@ -1,51 +1,47 @@
-import {
-  UpdateRoomResponse,
-  GameRoom,
-  GameCreateResponse,
-  PlayerInfo,
-} from './types.js';
+import { UpdateRoomResponse, Game, PlayerInfo, Room } from './types.js';
 import { users } from './db.js';
 
-export const rooms: GameCreateResponse[] = [];
-export const games = [];
-let currenIdRoom = 0;
-let currenIdGame = 0;
+export const rooms: Room[] = [];
+export const games: Game[] = [];
+let currenIdRoom: number = 0;
+let currenIdGame: number = 0;
 
 export function createRoom(userID: number): void {
   if (rooms.findIndex((room) => room.idPlayer === userID) < 0) {
-    rooms.push(<GameCreateResponse>{ idGame: currenIdRoom, idPlayer: userID });
+    const newRoom: Room = { idGame: currenIdRoom, idPlayer: userID };
+    rooms.push(newRoom);
     currenIdRoom++;
   }
 }
 
-export function createGame(usersID: number[]): number {
-  games.push(<GameRoom>{ idGame: currenIdGame, idPlayers: usersID });
+export function createGame(usersID: Record<number, number>): number {
+  const newGame: Game = { idGame: currenIdGame, idPlayers: usersID };
+  games.push(newGame);
   currenIdGame++;
   return currenIdGame - 1;
 }
 
 export function findUserInRoom(roomIndex: number): number {
   const roomInd = rooms.findIndex((room) => room.idGame === roomIndex);
-  let findingUserID = -1;
-  if (roomInd >= 0) {
-    findingUserID = rooms[roomInd].idPlayer;
-    delete rooms.at(roomInd);
-  }
+  if (roomInd < 0) throw new Error('Room not find!');
+  const findingUserID = rooms[roomInd].idPlayer;
+  delete rooms.at(roomInd);
   return findingUserID;
 }
 
-export function getAvailibleRooms(): UpdateRoomResponse[] {
-  const availibleRooms: UpdateRoomResponse[] = [];
-  rooms.forEach((room) => {
-    availibleRooms.push({
+export function getAvailableRooms(): UpdateRoomResponse[] {
+  const availableRooms: UpdateRoomResponse[] = [];
+  rooms.forEach((room: Room) => {
+    const playerInfo: PlayerInfo[] = [
+      {
+        name: users[room.idPlayer].name,
+        index: room.idPlayer,
+      },
+    ];
+    availableRooms.push({
       roomId: room.idGame,
-      roomUsers: [
-        {
-          name: users.at(room.idPlayer)?.name,
-          index: room.idPlayer,
-        } as PlayerInfo,
-      ],
+      roomUsers: playerInfo,
     } as UpdateRoomResponse);
   });
-  return [...availibleRooms];
+  return [...availableRooms];
 }
